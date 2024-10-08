6de6a8a56be7576a76698e0dd4cdb1f5790742be
nguyenngocnhan1 <129957403+nguyenngocnhan1@users.noreply.github.com>
2024-10-08 13:57:02 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+// Sử dụng biến môi trường để quản lý kết nối MongoD
+const MONGODB_URL='mongodb+srv://nhan123:nhan123@cluster0.v9orl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+
+
+// Sử dụng async/await để kết nối MongoDB
+const connectDB = async () => {
+    try {
+        await mongoose.connect(MONGODB_URL);
+        console.log('Connected to the database successfully');
+    } catch (error) {
+        console.error('Error connecting to the database', error);
+        process.exit(1); // Thoát ứng dụng nếu kết nối thất bại
+    }
+};
+
+// Gọi hàm connectDB để kết nối cơ sở dữ liệu
+connectDB();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+app.use(cors());
+
+const locationRoute = require('./api/routes/location.route')
+app.use("/v1/api/location",locationRoute)
+app.get('/',(req,res) => res.send("cupng dang bu"))
+// Kiểm tra nếu process.env.PORT không tồn tại thì sử dụng port 3000 mặc định
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+    
+});
